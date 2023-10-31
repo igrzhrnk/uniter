c7c0b9fdf2103aed8f95aad33e98d6cb5bb8ad4f
igrzhrnk <igorzhrnko@gmail.com>
2023-10-31 19:11:01 +0200

    refactor: add two new classes instead of old one

--- units/weight.py
+++ units/weight.py
@@ -1,28 +1,30 @@
-class Weight:
-    def __init__(self, amount: float, unit_name: str):
-        self.amount = amount
-        self.unit_name = unit_name
-    
-    
-    def __weight_units(self, u: str) -> int:
-        res = 0
-        match u:
-            case "g" | "gramm":
-                res = 1
-            case "kg" | "kilogram":
-                res = 3
-        
-        return res
+class Kilogram:
+    _name = "kilogram"
+    _alias = "kg"
+    _amount = 1000
 
+    def __init__(self, amount: float):
+        self.amount = amount
 
     def convert_to(self, unit_name: str):
-        res = 0.0
+        match unit_name:
+            case "kg" | "kilogram":
+                return self.amount
+            case "g" | "gramm":
+                return self.amount * self._amount
 
-        if self.__weight_units(unit_name) == self.__weight_units(self.unit_name):
-            res = self.amount
-        elif self.__weight_units(unit_name) < self.__weight_units(self.unit_name):
-            res = self.amount * (10**self.__weight_units(self.unit_name))
-        else:
-            res = self.amount / (10**self.__weight_units(unit_name))
 
-        return res
+class Gramm:
+    _name = "gramm"
+    _alias = "g"
+    _amount = 1000
+
+    def __init__(self, amount: float):
+        self.amount = amount
+
+    def convert_to(self, unit_name: str):
+        match unit_name:
+            case "g" | "gramm":
+                return self.amount
+            case "kg" | "kilogram":
+                return self.amount / self._amount
